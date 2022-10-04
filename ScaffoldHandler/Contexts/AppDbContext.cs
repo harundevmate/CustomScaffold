2556894272934b003e1e63181939ec99d65e34d9
lukman harun <lukman.harun@nttindonesia.com>
2022-10-04 10:03:06 +0700

    Update Scaffold

---
+++ ScaffoldHandler/Contexts/AppDbContext.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using ScaffoldHandler.Models;
+
+namespace ScaffoldHandler.Contexts
+{
+    public partial class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=localhost;Database=Learning;Integrated Security=False;User Id=dev;Password=P@ssw0rd;MultipleActiveResultSets=True");
+            }
+        }
+
+        public virtual DbSet<Item> Items { get; set; }
+        public virtual DbSet<UnitMeasure> UnitMeasures { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>(entity =>
+            {
+                entity.ToTable("Item");
+
+                entity.HasIndex(e => e.Id, "IX_ItemCode")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.UnitMeasureId, "IX_Item_UnitMeasureId");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Code)
+                    .IsRequired()
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.CreatedBy)
+                    .IsRequired()
+                    .HasMaxLength(500);
+
+                entity.Property(e => e.DeletedBy).HasMaxLength(100);
+
+                entity.Property(e => e.Description).HasMaxLength(500);
+
+                entity.Property(e => e.ModifiedBy).HasMaxLength(500);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(1000);
+
+                entity.HasOne(d => d.UnitMeasure)
+                    .WithMany(p => p.Items)
+                    .HasForeignKey(d => d.UnitMeasureId);
+            });
+
+            modelBuilder.Entity<UnitMeasure>(entity =>
+            {
+                entity.ToTable("UnitMeasure");
+
+                entity.HasIndex(e => e.Id, "IX_UnitMeasureCode")
+                    .IsUnique();
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Code)
+                    .IsRequired()
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.CreatedBy)
+                    .IsRequired()
+                    .HasMaxLength(500);
+
+                entity.Property(e => e.DeletedBy).HasMaxLength(100);
+
+                entity.Property(e => e.Description).HasMaxLength(500);
+
+                entity.Property(e => e.ModifiedBy).HasMaxLength(500);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(1000);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ ScaffoldHandler/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Shared;
+
+namespace ScaffoldHandler.Models
+{
+    public partial class Item : EntityBase
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public Guid UnitMeasureId { get; set; }
+
+        public virtual UnitMeasure UnitMeasure { get; set; }
+    }
+}
